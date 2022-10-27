5b306ce8412949d407daa49cad5a79f045ca13cc
SebastianKring <sebastian.kring@stud.bs7-augsburg.de>
2022-10-27 10:21:44 +0200

    g

--- Gradient Stuff/Node.cs
+++ Gradient Stuff/Node.cs
@@ -1,36 +1,22 @@
 ﻿using System.Numerics;
-using ILGPU.IR;
 
 namespace Gradient_Stuff
 {
     public struct Node
     {
-        private Vector3 _height;
-        private Vector3 _oHeight;
+        private float _height;
+        private float _oHeight;
 
-        public Vector3 Height
+        public float Height
         {
             get => _height;
-            set
-            {
-                value.X = value.X < 0 ? 0 : value.X > 255 ? 255 : value.X;
-                value.Y = value.Y < 0 ? 0 : value.Y > 255 ? 255 : value.Y;
-                value.Z = value.Z < 0 ? 0 : value.Z > 255 ? 255 : value.Z;
-                _height = value;
-            }
-                
+            set => _height = value < 0 ? 0 : value > 255 ? 255 : value;
         }
 
-        public Vector3 OHeight
+        public float OHeight
         {
             get => _oHeight;
-            set
-            {
-                value.X = value.X < 0 ? 0 : value.X > 255 ? 255 : value.X;
-                value.Y = value.Y < 0 ? 0 : value.Y > 255 ? 255 : value.Y;
-                value.Z = value.Z < 0 ? 0 : value.Z > 255 ? 255 : value.Z;
-                _oHeight = value;
-            }
+            set => _oHeight = value < 0 ? 0 : value > 255 ? 255 : value;
         }
     }
 }
